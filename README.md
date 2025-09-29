b43ed2e8f565bd99c87caaaa59165992fe894fc4
Kashifro <85724195+Kashifro@users.noreply.github.com>
2025-09-29 20:28:58 +0530

    #3

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# ShulkerPreview
+# CorvidShulker
 
 ## Overview
-ShulkerPreview is, well, my poor attempt at a mod, and rn it can kinda do the following stuff (idk how good tho lol):  
+CorvidShulker is, well, my poor attempt at a mod, and rn it can kinda do the following stuff (idk how good tho lol):  
 - show item name  
 - show count  
 - show aux value  
 - show some block refs (maybe)  
 - dump user NBT/extra stuff  
+
+Honestly idk what to do with it next and the reference pics aren’t even worth showing lol  
--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,29 @@
 #include <cstdio>
 #include <cstddef>
 #include <format>
+#include "main.h"
 #include <libhat.hpp>
 
 //fwd dec
-class ShulkerBoxBlockItem;
 class ItemStackBase;
+class ShulkerBoxBlockItem;
 class Level;
 
 
 //ptr type
-using ItemStackBase_toString_t = std::string(*)(ItemStackBase* self);
 using ItemStackBase_toDebugString_t = std::string(*)(ItemStackBase* self);
 using Shulker_appendHover_t = void(*)(ShulkerBoxBlockItem* self, const ItemStackBase& stack, Level& level, std::string& out, bool flag);
 
 //org ptr
-ItemStackBase_toString_t ItemStackBase_toString_orig = nullptr;
 ItemStackBase_toDebugString_t ItemStackBase_toDebugString_orig = nullptr;
 Shulker_appendHover_t ShulkerBoxBlockItem_appendFormattedHovertext_orig = nullptr;
 
-//hook string prolly don't need it 
-std::string ItemStackBase_toString_hook(ItemStackBase* self) {
-}
-
 //hook DebugString another mindless hook
 std::string ItemStackBase_toDebugString_hook(ItemStackBase* self) {
+     if (ItemStackBase_toDebugString_orig) {
+        return ItemStackBase_toDebugString_orig(self);
+    }
+    return "DebugString unavailable";
 }
 //hook append
 void ShulkerBoxBlockItem_appendFormattedHovertext_hook(ShulkerBoxBlockItem* self, const ItemStackBase& stack, Level& level, std::string& out, bool flag) {
@@ -37,22 +36,12 @@ void ShulkerBoxBlockItem_appendFormattedHovertext_hook(ShulkerBoxBlockItem* self
         ShulkerBoxBlockItem_appendFormattedHovertext_orig(self, stack, level, out, flag);
 
     out.append("\n§2Preview Goes Here\n");
-
-    // if (ItemStackBase_toString_orig) {
-    //     std::string itemInfo = ItemStackBase_toString_orig(const_cast<ItemStackBase*>(&stack));
-    //     out.append("\n§5Item ID: " + itemInfo);
-    // } else {
-    //     out.append("\n§5Item ID: unknown");
-    // }
       if (ItemStackBase_toDebugString_orig) {
         std::string debug = ItemStackBase_toDebugString_orig(const_cast<ItemStackBase*>(&stack));
-        out.append("\n§6NBT/Extra Info:\n" + debug); // too much extra info and well it kinda is string hook but well debug lol xD
+        out.append("\n§6Info:\n" + debug); // too much extra info and well it kinda is string hook but well debug lol xD
+        //rest of the logic
     }
-    //rest of the logic
-
 }
-
-
 extern "C" [[gnu::visibility("default")]] void mod_preinit() {}
 extern "C" [[gnu::visibility("default")]] void mod_init() {
     void* mcLib = dlopen("libminecraftpe.so", 0);
@@ -70,6 +59,7 @@ extern "C" [[gnu::visibility("default")]] void mod_init() {
         return (*static_cast<decltype(callback)*>(data))(*info);
     }, &callback);
 
+
     //ShulkerBoxBlockItem
     auto ZTS19ShulkerBoxBlockItem = hat::find_pattern(range1, hat::object_to_signature("19ShulkerBoxBlockItem")).get();
     auto _ZTI19ShulkerBoxBlockItem = hat::find_pattern(range2, hat::object_to_signature(ZTS19ShulkerBoxBlockItem)).get() - sizeof(void*);
@@ -79,27 +69,16 @@ extern "C" [[gnu::visibility("default")]] void mod_init() {
     ShulkerBoxBlockItem_appendFormattedHovertext_orig =
         reinterpret_cast<Shulker_appendHover_t>(vt0[53]);
     vt0[53] = reinterpret_cast<void*>(&ShulkerBoxBlockItem_appendFormattedHovertext_hook);
-    printf("[ShulkerPreview] Shulker hook installed\n"); //idk bro idk
+
 
     //ItemStackBase
     auto ZTS13ItemStackBase = hat::find_pattern(range1, hat::object_to_signature("13ItemStackBase")).get();
     auto _ZTI13ItemStackBase = hat::find_pattern(range2, hat::object_to_signature(ZTS13ItemStackBase)).get() - sizeof(void*);
     auto _ZTV13ItemStackBase = hat::find_pattern(range2, hat::object_to_signature(_ZTI13ItemStackBase)).get() + sizeof(void*);
-    
-
-    //slot5
-    
-    //slot6
-    void** vt1 = reinterpret_cast<void**>(_ZTV13ItemStackBase);
-    ItemStackBase_toString_orig =
-       reinterpret_cast<ItemStackBase_toString_t>(vt1[6]);
-    vt1[6] = reinterpret_cast<void*>(&ItemStackBase_toString_hook);
-    printf("[ShulkerPreview] Are we sure about this huh\n"); // another mindless out
-
     //slot7
      void** vt2 = reinterpret_cast<void**>(_ZTV13ItemStackBase);
     ItemStackBase_toDebugString_orig = 
        reinterpret_cast<ItemStackBase_toDebugString_t>(vt2[7]);
     vt2[7] = reinterpret_cast<void*>(&ItemStackBase_toDebugString_hook);
-    printf("[ShulkerPreview] Again???? \n "); //lol i gotta stop this goes nowhere
+
 }
